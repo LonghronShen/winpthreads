28573cd75f9b6b86b7c7cccce653409ae8d56019
Steven Hangger <LonghronShen@Hotmail.com>
2023-03-08 12:46:59 +0000

    fix: *
    *

--- src/misc.h
+++ src/misc.h
@@ -141,8 +141,8 @@ static WINPTHREADS_INLINE unsigned long dwMilliSecs(unsigned long long ms) {
 #endif
 
 unsigned long long _pthread_time_in_ms(void);
-unsigned long long _pthread_time_in_ms_from_timespec(const struct timespec *ts);
-unsigned long long _pthread_rel_time_in_ms(const struct timespec *ts);
+// unsigned long long _pthread_time_in_ms_from_timespec(const struct timespec *ts);
+// unsigned long long _pthread_rel_time_in_ms(const struct timespec *ts);
 unsigned long _pthread_wait_for_single_object(void *handle,
                                               unsigned long timeout);
 unsigned long _pthread_wait_for_multiple_objects(unsigned long count,
